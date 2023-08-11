05c345b6009e67dbb3d9b210fbd3c95178a05fd9
Michael Newton <miken32@gmail.com>
2023-08-11 15:35:29 -0600

    not sure how useful this is, but modernize it anyway

--- src/fastagi.php
+++ src/fastagi.php
@@ -1,79 +1,68 @@
-#!/usr/local/bin/php -q
+#!/usr/bin/env/php
 <?php
+namespace PhpAgi;
+
+if (!class_exists('PhpAgi\\AGI')) {
+    require_once('../vendor/autoload.php');
+}
+
 /**
- * phpagi-fastagi.php : PHP FastAGI bootstrap
- * @see https://github.com/welltime/phpagi
+ * PHP FastAGI bootstrap
+ *
+ *  This software is released under the terms of the GNU Lesser General Public License v2.1
+ *  a copy of which is available from http://www.gnu.org/copyleft/lesser.html
+ *
+ * @package PhpAgi
+ * @version 3.0
+ * @filesource https://github.com/welltime/phpagi
  * @filesource http://phpagi.sourceforge.net/
- *
- * $Id: phpagi-fastagi.php,v 1.2 2005/05/25 18:43:48 pinhole Exp $
- *
- * Copyright (c) 2004, 2005 Matthew Asham <matthewa@bcwireless.net>, David Eder <david@eder.us>
- * All Rights Reserved.
- *
- * This software is released under the terms of the GNU Lesser General Public License v2.1
- * A copy of which is available from http://www.gnu.org/copyleft/lesser.html
- *
- * We would be happy to list your phpagi based application on the phpagi
- * website.  Drop me an Email if you'd like us to list your program.
- *
- * @package phpAGI
- * @version 2.0
- * @example docs/fastagi.xinetd Example xinetd config file
+ * @copyright 2004 - 2010 Matthew Asham <matthew@ochrelabs.com>, David Eder <david@eder.us> and others
+ * @copyright 2023 RadiusOne Inc.
+ * @license LGPLv2.1
  */
-
-/**
- * Written for PHP 4.3.4, should work with older PHP 4.x versions.
- * Please submit bug reports, patches, etc to https://github.com/welltime/phpagi
- *
- */
-
-require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'phpagi.php');
 
 $fastagi = new AGI();
-
 $fastagi->verbose(print_r($fastagi, true));
-
-if(!isset($fastagi->config['fastagi']['basedir']))
-    $fastagi->config['fastagi']['basedir'] = dirname(__FILE__);
+$basedir = realpath($fastagi->config['fastagi']['basedir'] ?? dirname(__FILE__));
+$script = realpath($basedir . DIRECTORY_SEPARATOR . $fastagi->request['agi_network_script']);
 
 // perform some security checks
 
-$script = $fastagi->config['fastagi']['basedir'] . DIRECTORY_SEPARATOR . $fastagi->request['agi_network_script'];
-
 // in the same directory (or subdirectory)
-$mydir = dirname($fastagi->config['fastagi']['basedir']) . DIRECTORY_SEPARATOR;
-$dir = dirname($script) . DIRECTORY_SEPARATOR;
-if(substr($dir, 0, strlen($mydir)) != $mydir)
-{
-    $fastagi->conlog("$script is not allowed to execute.");
+if (!str_starts_with($script, $basedir)) {
+    $fastagi->conlog("$script is not located in $basedir.");
     exit;
 }
 
 // make sure it exists
-if(!file_exists($script))
-{
+if(!file_exists($script)) {
     $fastagi->conlog("$script does not exist.");
+
     exit;
 }
 
 // drop privileges
-if(isset($fastagi->config['fastagi']['setuid']) && $fastagi->config['fastagi']['setuid'])
-{
+if(extension_loaded('posix') && ($fastagi->config['fastagi']['setuid'] ?? false)) {
     $owner = fileowner($script);
     $group = filegroup($script);
-    if(!posix_setgid($group) || !posix_setegid($group) || !posix_setuid($owner) || !posix_seteuid($owner))
+    if(
+        !posix_setgid($group)
+        || !posix_setegid($group)
+        || !posix_setuid($owner)
+        || !posix_seteuid($owner)
+    )
     {
         $fastagi->conlog("failed to lower privileges.");
+
         exit;
     }
 }
 
 // make sure script is still readable
-if(!is_readable($script))
-{
+if(!is_readable($script)) {
     $fastagi->conlog("$script is not readable.");
+
     exit;
 }
 
 require_once($script);
-?>
